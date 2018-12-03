e4f7ab55de8b7e91b090ecabcab2835a7a292dda
CompBio <lbazar@lsu.edu>
2018-12-02 21:49:51 -0800

    COmplete - Final Object Script- Needs to be added to Main Script so that the complete Sequence Will Print Afterward.

--- ideas/FirstDraft/Try1.py
+++ ideas/FirstDraft/Try1.py
@@ -2,28 +2,37 @@
 
 class contig(object):
 	"""hold all short reads in contig"""
-	def  __init__(self):
+	def __init__(self,length):
+#list to hold reads
 		self.lists = []
 
+#short reads generated in file are read in from file
+
 file = open('shortread.txt', 'r')
-self.lists = []
+lists = []
 for reads in file.readlines():
 	lists.append(reads)
-self.lists[:] = [line.rstrip('\n') for line in self.lists]
-print(self.lists)
+lists[:] = [line.rstrip('\n') for line in lists]
+
 
 class fragmentRecord(object):
 	""" A class to define a short read within a contig."""
-	def __init__(self,sequence,length):
+	def __init__(self,sequence):
 		self.sequence = sequence
-		self.length = len(sequence)
 
 	def printRecord(self):
 		print("Sequence: " +self.sequence)
-		print("Length: " +self.length)
+
+
+Contig = contig(length="Length: ")
 
 print("---shortreadinformation---")
-for fragment in self.lists:
-	fragment = fragmentRecord(sequence=fragment,length= len(fragment))
+for fragment in lists:
+	fragment = fragmentRecord(sequence=fragment)
 	fragment.printRecord()
-	print()
+for fragment in lists:
+	print("Length of: ",end='')
+	print(fragment, end='')
+	print(" is ",end='')
+	print(len(fragment),end='')
+	print( " base pairs.")
