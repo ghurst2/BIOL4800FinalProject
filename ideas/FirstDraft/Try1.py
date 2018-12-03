4003719d1054530d726591e36e995e87d6f11bc5
ghurst2 <42451295+ghurst2@users.noreply.github.com>
2018-12-03 14:40:41 -0600

    update

--- ideas/FirstDraft/Try1.py
+++ ideas/FirstDraft/Try1.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 class contig(object):
-	"""hold all short reads in contig"""
-	def __init__(self,length):
-#list to hold reads
+	"""This class will hold all short reads within each contig, which is generated into seperate files"""
+	def __init__(self):
+#list to hold short reads per output file generated 
 		self.lists = []
 
-#short reads generated in file are read in from file
+#short reads generated in ouput file are read in from file and appended to lists
 
 file = open('shortread.txt', 'r')
 lists = []
@@ -14,7 +14,7 @@ for reads in file.readlines():
 	lists.append(reads)
 lists[:] = [line.rstrip('\n') for line in lists]
 
-
+#This class is to retain records on each fragment
 class fragmentRecord(object):
 	""" A class to define a short read within a contig."""
 	def __init__(self,sequence):
@@ -22,9 +22,11 @@ class fragmentRecord(object):
 
 	def printRecord(self):
 		print("Sequence: " +self.sequence)
-
-
-Contig = contig(length="Length: ")
+		
+#will now use printRecord def to apply info to fragmentRecord
+#set fragment equal to generic version of input to avoid manual input
+#use in for loop with list of short reads from class contig to obtain various types of information
+#program specific loop to receieve data desired in return and create new variables of interest
 
 print("---shortreadinformation---")
 for fragment in lists:
